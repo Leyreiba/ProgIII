6db1ff2cb6d43cf842ba59c65a5c6106682b0531
leyre <leyre@LAPTOP-9NNJID38>
2017-12-18 22:00:18 +0100

    jtables arregladas. seleccion de una row

--- ProyectoViajes/src/Ventanas/ResultadosBusquedaVuelos.java
+++ ProyectoViajes/src/Ventanas/ResultadosBusquedaVuelos.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollBar;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.TableModel;
 
 import BaseDeDatos.BD;
 
@@ -19,6 +20,7 @@ import java.awt.event.MouseListener;
 import java.awt.event.ActionEvent;
 import java.awt.GridLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 
 public class ResultadosBusquedaVuelos extends JFrame {
 
@@ -51,12 +53,13 @@ public class ResultadosBusquedaVuelos extends JFrame {
 		panelSur.add(btnComprar);
 		btnComprar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				int numFilaIda, numFilaVuelta;
 				//si no hay nada seleccionado aparece un mensaje de error //CORREGIR LO DE SELECCIONAR DE LA JTABLE!!!!!!!!!!!!
-				if(!tablaSurIda.isCellSelected(1, 1)){
+				if(tablaSurIda.getSelectedRow()==-1){
 					JOptionPane.showMessageDialog(null, "Necesita seleccionar una opción para su vuelo de ida");
 				}
 				//si en la tabla de vuelta no hay nada seleccionado, le da la opcion de seleccionar algo o ir a por el hotel
-				else if(!tablaSurVuelta.isCellSelected(1, 1)){
+				else if(tablaSurVuelta.getSelectedRow()==-1){
 					String[] opcion={"SI","NO"};
 					JOptionPane.showConfirmDialog(null, "¿Seguir sin seleccionar vuelo de vuelta?", "ALERTA", JOptionPane.YES_NO_OPTION);
 						if(opcion.equals("SI")){
@@ -75,10 +78,24 @@ public class ResultadosBusquedaVuelos extends JFrame {
 							
 						}
 						else{
+							
 							ResultadosBusquedaVuelos rv= new ResultadosBusquedaVuelos(origen, destino, dia, mes, anio, precioMin, precioMax, diaVuelta, mesVuelta, anioVuelta);
 							rv.setVisible(true);
 							dispose();
 						}
+				}else{
+					//{"ORIGEN","DESTINO","DURACION","PRECIO","HORA","DIA","MES","AÑO"};
+					numFilaIda = tablaSurIda.getSelectedRow();
+					numFilaVuelta = tablaSurVuelta.getSelectedRow();
+					TableModel modelo = tablaSurIda.getModel();
+					String origen = (String) modelo.getValueAt(numFilaIda, 0);
+					String destino = (String) modelo.getValueAt(numFilaIda, 1);
+					int duracion = (int) modelo.getValueAt(numFilaIda, 2);
+					int precio = (int) modelo.getValueAt(numFilaIda, 3);
+					int hora = (int) modelo.getValueAt(numFilaIda, 4);
+					int dia = (int) modelo.getValueAt(numFilaIda, 5);
+					int mes = (int) modelo.getValueAt(numFilaIda, 6);
+					int anio = (int) modelo.getValueAt(numFilaIda, 7);
 				}
 //				GestionCompra vp= new GestionCompra();
 //				vp.setVisible(true);
@@ -105,47 +122,56 @@ public class ResultadosBusquedaVuelos extends JFrame {
 		
 		JPanel panelDatos = new JPanel();
 		panelCentro.add(panelDatos);
+		panelDatos.setLayout(null);
+		
+		
 		
 		JLabel lblOrigen = new JLabel("Origen:");
+		lblOrigen.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+		lblOrigen.setBounds(18, 5, 61, 14);
 		panelDatos.add(lblOrigen);
 		
 		//volcar el valor elegido para el origen
 		JLabel lblOrig = new JLabel(origen);
-		panelDatos.add(lblOrig);
-		
-       
+		lblOrig.setBounds(87, 5, 55, 14);
+		panelDatos.add(lblOrig);       
 		
 		//volcar el valor elegido para la fecha origen
-		JLabel lblFechaOrig = new JLabel(String.valueOf(dia)+ "-" + mes + " - " +String.valueOf(anio));
+		JLabel lblFechaOrig = new JLabel(String.valueOf(dia)+"-"+mes+"-"+String.valueOf(anio));
+		lblFechaOrig.setBounds(147, 5, 103, 14);
 		panelDatos.add(lblFechaOrig);
 		
+		
+		
 		JLabel lblDestino = new JLabel("Destino:");
+		lblDestino.setFont(new Font("Segoe UI Black", Font.PLAIN, 11));
+		lblDestino.setBounds(18, 31, 55, 14);
 		panelDatos.add(lblDestino);
 		
 		//volcar el valor elegido para el destino
 		JLabel lblDest = new JLabel(destino);
-		panelDatos.add(lblDest);
-		
+		lblDest.setBounds(87, 31, 55, 14);
+		panelDatos.add(lblDest);		
 		
 		//volcar el valor elegido para la fecha destino (respecto a los datos que hayamos introducido en el calendario de vuelta)
-		//quiero crear una opción que cuando le digamos que solo queremos vuelo de ida pponga a no visible estos datos de vuelta y el calendario de vuelta en la ventana anterior
+		//quiero crear una opción que cuando le digamos que solo queremos vuelo de ida ponga a no visible estos datos de vuelta y el calendario de vuelta en la ventana anterior
 		JLabel lblFechaDest = new JLabel(String.valueOf(diaVuelta)+"-"+mesVuelta+"-"+String.valueOf(anioVuelta));
+		lblFechaDest.setBounds(147, 31, 75, 14);
 		panelDatos.add(lblFechaDest);
+		
 
 		
 		JPanel panelTabla = new JPanel();
 		panelCentro.add(panelTabla);
-		panelTabla.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panelTabla.setLayout(new GridLayout(0, 1));
 		
 		//creo 2 paneles nuevos dentro del anterior para meter cada jtable en cada uno
 		
-		JPanel panelTablaEn2Izq = new JPanel();
-		panelTabla.add(panelTablaEn2Izq);
-		panelTablaEn2Izq.setLayout(new FlowLayout(FlowLayout.LEFT, 2, 2));
+		JPanel panelTablaArriba = new JPanel();
+		panelTabla.add(panelTablaArriba);
 		
-		JPanel panelTablaEn2Der = new JPanel();
-		panelTabla.add(panelTablaEn2Der);
-		panelTablaEn2Der.setLayout(new FlowLayout(FlowLayout.RIGHT, 2, 2));
+		JPanel panelTablaAbajo = new JPanel();
+		panelTabla.add(panelTablaAbajo);
 		
 		/**
 		 * Creamos una JTable con un array que mostraremos en la primera fila como muestra de los titulos
@@ -155,24 +181,25 @@ public class ResultadosBusquedaVuelos extends JFrame {
 		
 		
 		
-		String nombresColumnasIda[] = {"ORIGEN","DESINO","DURACION","PRECIO","HORA","DIA","MES","AÑO"}; //array con los titulos de cada columna
+		String nombresColumnasIda[] = {"ORIGEN","DESTINO","DURACION","PRECIO","HORA","DIA","MES","AÑO"}; //array con los titulos de cada columna
 		Object datosIda[][] = BD.volcarDatosVuelos(origen, destino, dia, mes, anio,precioMin, precioMax); //array donde voy a volcar los datos de la bd que se correspondan con los datos seleccionados anteriormente
 		tablaSurIda = new JTable(datosIda,nombresColumnasIda); //metemos en una jtable estos arrays
-		panelTablaEn2Izq.setLayout(new BorderLayout());
-		panelTablaEn2Izq.add(tablaSurIda.getTableHeader(), BorderLayout.NORTH); 
-		panelTablaEn2Izq.add(tablaSurIda, BorderLayout.NORTH);
+		panelTablaArriba.setLayout(new BorderLayout());
+		panelTablaArriba.add(tablaSurIda.getTableHeader(), BorderLayout.NORTH); 
+		panelTablaArriba.add(tablaSurIda, BorderLayout.CENTER);
 		JScrollBar sbIda= new JScrollBar();
 		tablaSurIda.add(sbIda);
 
-		
+		//el origen en este caso va a ser el destino y viceversa, porque es la jtable de vuelta
        String nombrescolumnasVuelta[]= {"ORIGEN", "DESTINO", "DURACION", "PRECIO", "HORA", "DIA", "MES", "AÑO"};
-       Object datosVuelta[][] = BD.volcarDatosVuelos(origen, destino, dia, mes, anio, precioMin, precioMax);
+       Object datosVuelta[][] = BD.volcarDatosVuelos(destino, origen, dia, mes, anio, precioMin, precioMax);
        tablaSurVuelta = new JTable(datosVuelta, nombrescolumnasVuelta);
-       panelTablaEn2Der.setLayout(new BorderLayout());
-       panelTablaEn2Der.add(tablaSurVuelta.getTableHeader(), BorderLayout.SOUTH);
-       panelTablaEn2Der.add(tablaSurVuelta, BorderLayout.SOUTH);
+       panelTablaAbajo.setLayout(new BorderLayout());
+       panelTablaAbajo.add(tablaSurVuelta.getTableHeader(), BorderLayout.NORTH);
+       panelTablaAbajo.add(tablaSurVuelta, BorderLayout.CENTER);
        JScrollBar sbVuelta= new JScrollBar();
        tablaSurVuelta.add(sbVuelta);
        
+   	
 	}
 }
